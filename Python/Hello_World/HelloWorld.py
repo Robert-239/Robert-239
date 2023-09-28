b2e04797cc57f5ae57b488fd2302f0d22beb20a3
Robert Wilson <robertwilson02@icloud.com>
2023-09-28 15:10:39 +0200

    Add computer science 414 work

--- Python/Hello_World/HelloWorld.py
+++ Python/Hello_World/HelloWorld.py
@@ -1,1 +1,11 @@
+import numpy as np 
+from matplotlib import pyplot as plt
+
 print('hello world')
+j = 12
+def randomArray(n):
+    arr = np.array([])
+    for i in range(1,n+1):
+        arr[i] = np.random
+    return arr
+print(randomArray(j))
